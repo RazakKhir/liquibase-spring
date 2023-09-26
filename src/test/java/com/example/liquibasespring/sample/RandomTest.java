a22a5cc86a6941a28308d5dff6be7f2e97bbf35a
RazakKhir <razak.khir@yahoo.com>
2023-09-26 22:57:35 +0800

    random testing lol

--- src/test/java/com/example/liquibasespring/sample/RandomTest.java
+++ src/test/java/com/example/liquibasespring/sample/RandomTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import static org.junit.Assert.assertNotNull;
@@ -33,4 +34,21 @@ public class RandomTest {
         log.info("date: {}", dateFormatFinal);
         assertNotNull(dateFormatFinal);
     }
+
+    @Test
+    @DisplayName("test convert millisecond to joda datetime")
+    void testConvertToJoda(){
+        DateTime dt = DateTime.parse("2018-09-06T00:00:00");
+        log.info("datetime: {}", dt);
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, 0);
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.SECOND, 0);
+        c.set(Calendar.MILLISECOND, 0);
+        c.setTimeInMillis(dt.getMillis());
+        c.add(Calendar.MONTH, 3);
+        DateTime dtResult = new DateTime(c.getTimeInMillis());
+        log.info("dt result: {}", dtResult);
+    }
+
 }
